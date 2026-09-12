7dff22e8568ef7d3cb29d054d257c25ce017a609
Marcos <marcosmallet@gmail.com>
2026-09-12 02:41:06 -0300

    chore: expose Playwright failure for diagnosis

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  reporter: process.env.CI
+    ? [['json', { outputFile: 'test-results.json' }]]
+    : undefined,
   use: {
     baseURL: 'http://127.0.0.1:4173',
     headless: true
